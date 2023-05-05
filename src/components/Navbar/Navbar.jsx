79ac0aecf39cdb04f9715b6123ebbe69a518c776
MathiasDininno <dininnom1@gmail.com>
2023-05-05 17:00:19 -0300

    Cambios de prueba.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -11,12 +11,12 @@ const Navbar = () => {
           <div className="collapse navbar-collapse" id="navbarColor03">
             <ul className="navbar-nav me-auto">
               <li className="nav-item">
-                <a className="nav-link active" href="#">Home
+                <a className="nav-link active" href="#">Hola
                   <span className="visually-hidden">(current)</span>
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Features</a>
+                <a className="nav-link" href="#">Calden</a>
               </li>
               <li className="nav-item">
                 <a className="nav-link" href="#">Pricing</a>
