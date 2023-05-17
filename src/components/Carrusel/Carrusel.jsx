f72cbb1fcc9d92eaa17e4c4233ab4e81a01994db
Alex <alexdiazvera97@gmail.com>
2023-05-17 18:31:55 -0300

    navbar

---
+++ src/components/Carrusel/Carrusel.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../Carrusel/carrusel.css'
+
+const Carrusel = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Carrusel;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,40 +1,31 @@
 import React from 'react';
+import '../Navbar/navbar.css'
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light p-0">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">Navbar</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon" />
           </button>
-          <div className="collapse navbar-collapse" id="navbarColor03">
-            <ul className="navbar-nav me-auto">
+          <div className="collapse navbar-collapse navbarCalden" id="navbarColor03">
+            <ul className="navbar-nav">
               <li className="nav-item">
-                <a className="nav-link active" href="#">Hola
-                  <span className="visually-hidden">(current)</span>
-                </a>
+                <a className="nav-link" href="#">Acerca</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Calden</a>
+                <a className="nav-link" href="#">Galeria</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Pricing</a>
+                <a className="nav-link" href="#">Contacto</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">About</a>
-              </li>
-              <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Categorias</a>
-                <div className="dropdown-menu">
-                  <a className="dropdown-item" href="#"></a>
-                  <a className="dropdown-item" href="#">Another action</a>
-                  <a className="dropdown-item" href="#">Something else here</a>
-                  <div className="dropdown-divider" />
-                  <a className="dropdown-item" href="#">Separated link</a>
-                </div>
+                <a className="nav-link" href="#">Tienda</a>
               </li>
             </ul>
+            <div>
+            <img className='caldenLogo' src="./img/logo/caldenLogo.png" alt="CaldenLogotipo"/>
+            </div>
           </div>
         </div>
       </nav>
